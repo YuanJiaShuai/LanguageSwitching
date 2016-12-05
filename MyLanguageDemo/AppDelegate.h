e264ae25fc4e2a02299163483bbeecf2a7f4c8d7
yjs_maoge@163.com <apple@appledemacbook-air.local>
2016-12-05 14:56:18 +0800

    Initial Commit

---
+++ MyLanguageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyLanguageDemo
+//
+//  Created by apple on 16/12/5.
+//  Copyright © 2016年 YJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyLanguageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyLanguageDemo
+//
+//  Created by apple on 16/12/5.
+//  Copyright © 2016年 YJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
